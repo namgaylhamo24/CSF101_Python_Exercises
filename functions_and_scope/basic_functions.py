52ac86a3808b8ca90da3a87a521974ecde5349f4
namgaylhamo24 <02240350.cst@rub.edu.bt>
2024-08-31 08:51:28 +0600

    Functions

---
+++ functions_and_scope/basic_functions.py
@@ -0,0 +1,32 @@
+def function_name(parameter1, parameter2, parameter3):
+    return value 
+
+def greet():
+    print("Hello world!")
+greet()
+
+def greet_person(name):
+    print(f"Hello,{name}")
+greet_person("Agyeto")
+
+def square(numbers):
+    return numbers ** 2
+result= square(5)
+print(result)
+
+def is_even(numbers):
+    return numbers % 2 == 0
+print(is_even(4))
+print(is_even(7))
+
+def print_numbers (n):
+    for i in range(1, n+1):
+        print (i)
+print_numbers(5)
+
+
+
+
+
+
+
